aacd212f0de7940edd0c03d8a9cc7391ef63daf5
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2015-03-02 16:23:51 +0100

    Fix

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-cp --no-clobber other/com.lightheadsw.Caffeine.plist ~/Library/Preferences/com.lightheadsw.Caffeine.plist
-cp --no-clobber other/org.herf.Flux.plist ~/Library/Preferences/org.herf.Flux.plist
-cp --no-clobber other/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
-cp --no-clobber other/com.torusknot.SourceTreeNotMAS.plist ~/Library/Preferences/com.torusknot.SourceTreeNotMAS.plist
+cp other/com.lightheadsw.Caffeine.plist ~/Library/Preferences/com.lightheadsw.Caffeine.plist
+cp other/org.herf.Flux.plist ~/Library/Preferences/org.herf.Flux.plist
+cp other/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
+cp other/com.torusknot.SourceTreeNotMAS.plist ~/Library/Preferences/com.torusknot.SourceTreeNotMAS.plist
 killall cfprefsd
 
 rcup -f -x install.sh -x "other/*"
@@ -17,11 +17,11 @@ mkdir -p ~/.vim/syntax
 mkdir -p ~/.vim/undo
 
 mkdir -p "$HOME/Library/Application Support/Sublime Text 3/Packages/User/"
-cp --no-clobber "other/sublime/Default (OSX).sublime-keymap" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Default (OSX).sublime-keymap"
-cp --no-clobber "other/sublime/Package Control.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Package Control.sublime-settings"
-cp --no-clobber "other/sublime/Preferences.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings"
-cp --no-clobber "other/sublime/Ruby on Rails.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Ruby on Rails.sublime-settings"
-cp --no-clobber "other/sublime/Shell-Unix-Generic.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Shell-Unix-Generic.sublime-settings"
-cp --no-clobber "other/sublime/Textile.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Textile.sublime-settings"
-cp --no-clobber "other/sublime/SublimeLinter.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/SublimeLinter.sublime-settings"
-cp --no-clobber "other/sublime/typescript.sublime-build" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/typescript.sublime-build"
+cp "other/sublime/Default (OSX).sublime-keymap" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Default (OSX).sublime-keymap"
+cp "other/sublime/Package Control.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Package Control.sublime-settings"
+cp "other/sublime/Preferences.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings"
+cp "other/sublime/Ruby on Rails.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Ruby on Rails.sublime-settings"
+cp "other/sublime/Shell-Unix-Generic.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Shell-Unix-Generic.sublime-settings"
+cp "other/sublime/Textile.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Textile.sublime-settings"
+cp "other/sublime/SublimeLinter.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/SublimeLinter.sublime-settings"
+cp "other/sublime/typescript.sublime-build" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/typescript.sublime-build"
